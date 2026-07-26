1601a539053690f91f8ad70c90ccf767e178bc5d
Adevar-nz <st23502@howick.school.nz>
2026-07-27 09:49:30 +1200

    I added functions and did testing
    I added various functions to help improve the program
    27/07/2026

--- Untitled-2.py
+++ Untitled-2.py
@@ -4,25 +4,26 @@ The purpose of this file is to enter students name and raw score
 then convert score to grade and display a neat table
 '''
 
-#dictionaries and Constants
+#dictionaries, Constants and global variables
 LOWER_GRADE_LIMIT = 0
 HIGHER_GRADE_LIMIT = 100
-TEACHER_NAME = "MS Liang"
-TEST_NAME = "Y10 Web design"
 
-Students_and_score = {}
+teacher_name = ""
+test_name = ""
+students_and_score = {}
 
 #--------Functions----------#
 
 def str_validator(string):
     ''' checks if the input is a string'''
 
-    if string.isalpha():
+    if string.replace(" ", "").isalpha():
         return True
     
     else:
         return False
     
+
 def number_and_range_validator(input, lower_range, higher_range):
     ''' Checks if the input is a number and if it is in the range'''
 
@@ -58,6 +59,57 @@ def grade_converter(score):
         return False
     
 
-def data_entering():
-    '''enters the data of the students and their score into the dictionary'''
+def entering_test_data():
+    '''enters the data for the teacher and test name'''
+
+    teacher_name = input("Please enter the teachers Name: ").title()
+
+    while str_validator(teacher_name) == False:
+        teacher_name = input("Please enter the teachers Name without numbers \
+or special characters: ").title()
+    
+    test_name = input('Please enter the test Name:')
+
+    while str_validator(test_name) == False:
+        test_name = input('Please enter the test Name without numbers or \
+ special characters:')
+
+    return [teacher_name, test_name]
+
+def score_entering():
+    ''' Enters the score for each student and when finish is typed it will
+    stop the program'''
+
+    finish = ''
+    
+    while finish != "Finish":
+
+        #Verifies the student name
+        student_name = input("Please enter the students Name: ").title()
+        while str_validator(student_name) == False:
+            student_name = input("Please enter a VALID student Name: ")
+        print()
+
+        #Verifies the student score
+        student_score = input("Please enter the students score: ")
+        while number_and_range_validator(student_score, LOWER_GRADE_LIMIT, \
+                                          HIGHER_GRADE_LIMIT) == False:
+            student_score = input("Please enter a VALID student score: ")
+        student_score = int(student_score)
+        print()
+
+        students_and_score[student_name] = student_score
+
+        finish = input("Would you like to carry on? type finish if no: ").title()
+        while str_validator(finish) == False:
+            finish = input("Would you like to carry on? type finish if no: ").title()
+        print()
             
+def average_grade(dictionary):
+    ''' gets the average score of the entire class'''
+
+    
+
+def get_highest_students():
+    ''' gets the names of the top 3 students '''
+
